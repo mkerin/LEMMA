86603db0570cbcd9205d7792116375e4da00fa90
mkerin <m4tt.kerin@gmail.com>
2018-05-31 16:24:57 +0100

    More accurate profiling of the update hyps step

--- test_updates.cpp
+++ test_updates.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "src/vbayes_tracker.hpp"
+#include "src/vbayes_x2.hpp"
 #include "src/genotype_matrix.hpp"
 #include "src/tools/eigen3.3/Dense"
 #include <random>
@@ -13,35 +15,17 @@ inline double sigmoid(double x){
 	return 1.0 / (1.0 + std::exp(-x));
 }
 
+void updateAlphaMu(const GenotypeMatrix& X,
+                   const Eigen::Ref<Eigen::VectorXd>& dHtH,
+                   const Eigen::Ref<Eigen::VectorXd>& Hty,
+                   const std::vector< std::uint32_t >& iter,
+                   const std::uint32_t& L,
+                   const Hyps& i_hyps,
+                   FreeParameters& i_par);
+
 long int N, P;
 int mode;
 
-namespace performance {
-    int parseLineRAM(char* line){
-        // This assumes that a digit will be found and the line ends in " Kb".
-        int i = strlen(line);
-        const char* p = line;
-        while (*p <'0' || *p > '9') p++;
-        line[i-3] = '\0';
-        i = atoi(p);
-        return i;
-    }
-
-    int getValueRAM(){ //Note: this value is in KB!
-        FILE* file = fopen("/proc/self/status", "r");
-        int result = -1;
-        char line[128];
-
-        while (fgets(line, 128, file) != NULL){
-            if (strncmp(line, "VmRSS:", 6) == 0){
-                result = parseLineRAM(line);
-                break;
-            }
-        }
-        fclose(file);
-        return result;
-    }
-}
 
 bool read(){
 	std::cout << "Input N:" << std::endl;
@@ -73,13 +57,20 @@ int main() {
 			break;
 		}
 
+		// Constants
+		double eps = std::numeric_limits<double>::min();
+		Eigen::Vector dHtH = Eigen::VectorXd::Random(2*P);
+		Eigen::Vector Hty  = Eigen::VectorXd::Random(2*P);
+		Hyps hyps;
+		hyps.lam_g = 0.001;
+		hyps.lam_b = 0.005;
+		hyps.sigma_g = 2.1;
+		hyps.sigma_b = 1.9;
+		hyps.sigma = 1.0;
+
 		// Eigen::MatrixXd G;
 		GenotypeMatrix X((bool) mode); // 0 -> false, else true
-		Eigen::VectorXd aa(N), rr(P);
-		for (long int jj = 0; jj < P; jj++){
-			rr[jj] = gaussian(gen_gauss);
-		}
-
+		Eigen::VectorXd aa(N);
 		X.resize(N, P);
 		for (long int ii = 0; ii < N; ii++){
 			for (long int jj = 0; jj < P; jj++){
@@ -90,81 +81,130 @@ int main() {
 		X.aa = aa;
 		X.calc_scaled_values();
 
-		Eigen::VectorXd alpha(2*P), mu(2*P), Hr(N), new_Hr(N);
-		for (long int jj = 0; jj < P; jj++){
-			alpha[jj] = standard_uniform(gen_unif);
-			mu[jj] = standard_uniform(gen_gauss);
+		// Free params
+		FreeParameters i_par;
+		i_par.Hr = Eigen::VectorXd::Random(N);
+		i_par.alpha.resize(2*P);
+		i_par.mu.resize(2*P);
+		i_par.s_sq.resize(2*P);
+		for (long int jj = 0; jj < 2*P; jj++){
+			i_par.alpha[jj] = standard_uniform(gen_unif);
+			i_par.mu[jj] = standard_uniform(gen_gauss);
 		}
-		for (long int ii = 0; ii < N; ii++){
-			Hr[ii] = standard_uniform(gen_gauss);
+		for (std::uint32_t kk = 0; kk < P; kk++){
+			i_par.s_sq[kk] = i_hyps.sigma_b * i_hyps.sigma / (i_hyps.sigma_b * dHtH(kk) + 1.0);
+		}
+		for (std::uint32_t kk = P; kk < 2*P; kk++){
+			i_par.s_sq[kk] = i_hyps.sigma_g * i_hyps.sigma / (i_hyps.sigma_g * dHtH(kk) + 1.0);
 		}
 
 		std::cout << "Data initialised" << std::endl;
-
-
-
-		double lam_g = 0.001, lam_b = 0.005, sigma_g = 2.1, sigma_b = 1.9, sigma = 1.0;
-		double eps = std::numeric_limits<double>::min();
-
-		// Cba to track
-		double dHtH = 1.2, Hty = 23.0, s_sq = 0.9;
 
 
 		// Calling matrix * vector multiplication
 		std::cout << "Testing updateAlphaMu method" << std::endl;
 		auto now = std::chrono::system_clock::now();
 
-		double rr_k, ff_k;
-		for(std::uint32_t kk = 0; kk < P; kk++){
-			rr_k = alpha(kk) * mu(kk);
-
-			// Update mu (eq 9)
-			mu(kk) = s_sq * (Hty - Hr.dot(X.col(kk)) + dHtH * rr_k) / sigma;
-
-			// Update alpha (eq 10)  TODO: check syntax / i_  / sigmoid here!
-			if (kk < P){
-				ff_k = std::log(lam_b / (1.0 - lam_b) + eps) + std::log(s_sq / sigma_b / sigma + eps) / 2.0;
-				ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
-			} else {
-				ff_k = std::log(lam_g / (1.0 - lam_g) + eps) + std::log(s_sq / sigma_g / sigma + eps) / 2.0;
-				ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
-			}
-			alpha(kk) = sigmoid(ff_k);
-
-			// Update i_Hr; faster to take schur product with aa inside genotype_matrix
-			new_Hr = Hr + (alpha(kk)*mu(kk) - rr_k) * (X.col(kk));
-		}
+		updateAlphaMu(X, dHtH, Hty, iter, 2*P, i_hyps, i_par);
 
 		auto end = std::chrono::system_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end-now;
-		std::cout << "Mean update time (main, " << P << " calls): " << elapsed_seconds.count() / P << std::endl;
-		std::cout << "Est. update time for P = 600k: " << elapsed_seconds.count() / P * 600000 << std::endl;
-
-		now = std::chrono::system_clock::now();
-		for(std::uint32_t kk = P; kk < 2*P; kk++){
-			rr_k = alpha(kk) * mu(kk);
-
-			// Update mu (eq 9)
-			mu(kk) = s_sq * (Hty - Hr.dot(X.col(kk)) + dHtH * rr_k) / sigma;
-
-			// Update alpha (eq 10)  TODO: check syntax / i_  / sigmoid here!
-			if (kk < P){
-				ff_k = std::log(lam_b / (1.0 - lam_b) + eps) + std::log(s_sq / sigma_b / sigma + eps) / 2.0;
-				ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
-			} else {
-				ff_k = std::log(lam_g / (1.0 - lam_g) + eps) + std::log(s_sq / sigma_g / sigma + eps) / 2.0;
-				ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
-			}
-			alpha(kk) = sigmoid(ff_k);
-
-			// Update i_Hr; faster to take schur product with aa inside genotype_matrix
-			new_Hr = Hr + (alpha(kk)*mu(kk) - rr_k) * (X.col(kk));
-		}
-		end = std::chrono::system_clock::now();
-		elapsed_seconds = end-now;
-		std::cout << "Mean update time (interaction, " << P << " calls): " << elapsed_seconds.count() / P << std::endl;
-		std::cout << "Est. update time for P = 600k: " << elapsed_seconds.count() / P * 600000 << std::endl;
+		std::cout << "Mean update time (" << P << " calls): " << elapsed_seconds.count() / P << std::endl;
+		std::cout << "Est. update time for P = 640k: " << elapsed_seconds.count() / P * 640000 << std::endl;
 	}
 
 	return 0;
 }
+
+
+void updateAlphaMu(const GenotypeMatrix& X,
+                   const Eigen::Ref<Eigen::VectorXd>& dHtH,
+                   const Eigen::Ref<Eigen::VectorXd>& Hty,
+                   const std::vector< std::uint32_t >& iter,
+                   const std::uint32_t& L,
+                   const Hyps& i_hyps,
+                   FreeParameters& i_par){
+	const eps = std::numeric_limits<double>::min();
+	std::uint32_t kk;
+	double rr_k, ff_k;
+	Eigen::VectorXd X_kk(X.NN);
+	for(std::uint32_t jj = 0; jj < L; jj++){
+		kk = iter[jj];
+		assert(kk < L);
+
+		rr_k = i_par.alpha(kk) * i_par.mu(kk);
+		X_kk = X.col(kk);
+		// Update mu (eq 9); faster to take schur product with aa inside genotype_matrix
+		i_par.mu(kk) = i_par.s_sq[kk] * (Hty(kk) - i_par.Hr.dot(X_kk) + dHtH(kk) * rr_k) / i_hyps.sigma;
+
+		// Update alpha (eq 10)  TODO: check syntax / i_  / sigmoid here!
+		if (kk < P){
+			ff_k = std::log(i_hyps.lam_b / (1.0 - i_hyps.lam_b) + eps) + std::log(i_par.s_sq[kk] / i_hyps.sigma_b / i_hyps.sigma + eps) / 2.0;
+			ff_k += i_par.mu(kk) * i_par.mu(kk) / i_par.s_sq[kk] / 2.0;
+		} else {
+			ff_k = std::log(i_hyps.lam_g / (1.0 - i_hyps.lam_g) + eps) + std::log(i_par.s_sq[kk] / i_hyps.sigma_g / i_hyps.sigma + eps) / 2.0;
+			ff_k += i_par.mu(kk) * i_par.mu(kk) / i_par.s_sq[kk] / 2.0;
+		}
+		i_par.alpha(kk) = sigmoid(ff_k);
+
+		// Update i_Hr; faster to take schur product with aa inside genotype_matrix
+		i_par.Hr = i_par.Hr + (i_par.alpha(kk)*i_par.mu(kk) - rr_k) * X_kk;
+	}
+}
+
+
+	// 	// Calling matrix * vector multiplication
+	// 	std::cout << "Testing updateAlphaMu method" << std::endl;
+	// 	auto now = std::chrono::system_clock::now();
+	// 
+	// 	double rr_k, ff_k;
+	// 	for(std::uint32_t kk = 0; kk < P; kk++){
+	// 		rr_k = alpha(kk) * mu(kk);
+	// 
+	// 		// Update mu (eq 9)
+	// 		mu(kk) = s_sq * (Hty - Hr.dot(X.col(kk)) + dHtH * rr_k) / sigma;
+	// 
+	// 		// Update alpha (eq 10)  TODO: check syntax / i_  / sigmoid here!
+	// 		if (kk < P){
+	// 			ff_k = std::log(lam_b / (1.0 - lam_b) + eps) + std::log(s_sq / sigma_b / sigma + eps) / 2.0;
+	// 			ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
+	// 		} else {
+	// 			ff_k = std::log(lam_g / (1.0 - lam_g) + eps) + std::log(s_sq / sigma_g / sigma + eps) / 2.0;
+	// 			ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
+	// 		}
+	// 		alpha(kk) = sigmoid(ff_k);
+	// 
+	// 		// Update i_Hr; faster to take schur product with aa inside genotype_matrix
+	// 		new_Hr = Hr + (alpha(kk)*mu(kk) - rr_k) * (X.col(kk));
+	// 	}
+	// 
+	// 	auto end = std::chrono::system_clock::now();
+	// 	std::chrono::duration<double> elapsed_seconds = end-now;
+	// 	std::cout << "Mean update time (main, " << P << " calls): " << elapsed_seconds.count() / P << std::endl;
+	// 	std::cout << "Est. update time for P = 600k: " << elapsed_seconds.count() / P * 600000 << std::endl;
+	// 
+	// 	now = std::chrono::system_clock::now();
+	// 	for(std::uint32_t kk = P; kk < 2*P; kk++){
+	// 		rr_k = alpha(kk) * mu(kk);
+	// 
+	// 		// Update mu (eq 9)
+	// 		mu(kk) = s_sq * (Hty - Hr.dot(X.col(kk)) + dHtH * rr_k) / sigma;
+	// 
+	// 		// Update alpha (eq 10)  TODO: check syntax / i_  / sigmoid here!
+	// 		if (kk < P){
+	// 			ff_k = std::log(lam_b / (1.0 - lam_b) + eps) + std::log(s_sq / sigma_b / sigma + eps) / 2.0;
+	// 			ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
+	// 		} else {
+	// 			ff_k = std::log(lam_g / (1.0 - lam_g) + eps) + std::log(s_sq / sigma_g / sigma + eps) / 2.0;
+	// 			ff_k += mu(kk) * mu(kk) / s_sq / 2.0;
+	// 		}
+	// 		alpha(kk) = sigmoid(ff_k);
+	// 
+	// 		// Update i_Hr; faster to take schur product with aa inside genotype_matrix
+	// 		new_Hr = Hr + (alpha(kk)*mu(kk) - rr_k) * (X.col(kk));
+	// 	}
+	// 	end = std::chrono::system_clock::now();
+	// 	elapsed_seconds = end-now;
+	// 	std::cout << "Mean update time (interaction, " << P << " calls): " << elapsed_seconds.count() / P << std::endl;
+	// 	std::cout << "Est. update time for P = 600k: " << elapsed_seconds.count() / P * 600000 << std::endl;
+	// }
