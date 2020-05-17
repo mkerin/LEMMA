9da103c58cde65a8150ece7b9704db1f9cf36519
mkerin <m4tt.kerin@gmail.com>
2020-05-17 10:05:29 +0100

    Direct gplemma tests to use simulated data

--- unit/test_gplemma.cpp
+++ unit/test_gplemma.cpp
@@ -20,7 +20,7 @@ char* argv_rhe_nm[] = { (char*) "LEMMA",
 					  (char*) "--streamBgen", (char*) "unit/data/n50_p100.bgen",
 					  (char*) "--pheno", (char*) "unit/data/pheno.txt",
 					  (char*) "--environment", (char*) "unit/data/n50_p100_env.txt",
-					  (char*) "--out", (char*) "data/io_test/test_RHEreg_NM.out.gz"};
+					  (char*) "--out", (char*) "unit/data/test_pve/RHEreg_NM.out.gz"};
 
 char* argv_rhe_lm2[] = { (char*) "LEMMA",
 						(char*) "--RHEreg-LM",
@@ -30,19 +30,19 @@ char* argv_rhe_lm2[] = { (char*) "LEMMA",
 						(char*) "--random-seed", (char*) "1",
 						(char*) "--n-RHEreg-jacknife", (char*) "1",
 						(char*) "--n-RHEreg-samples", (char*) "5",
-						(char*) "--streamBgen", (char*) "data/io_test/n1000_p2000.bgen",
-						(char*) "--pheno", (char*) "data/io_test/case8/pheno.txt",
-						(char*) "--environment", (char*) "data/io_test/case8/env.txt",
-						(char*) "--out", (char*) "data/io_test/test_RHEreg_LM.out.gz"};
+						(char*) "--streamBgen", (char*) "unit/data/n50_p100.bgen",
+						(char*) "--pheno", (char*) "unit/data/pheno.txt",
+						(char*) "--environment", (char*) "unit/data/n50_p100_env.txt",
+						(char*) "--out", (char*) "unit/data/test_pve/RHEreg_LM.out.gz"};
 
 char* argv_multiE[] = { (char*) "--RHEreg", (char*) "--maf", (char*) "0.01",
 						(char*) "--random-seed", (char*) "1",
 						(char*) "--n-RHEreg-jacknife", (char*) "1",
 						(char*) "--n-RHEreg-samples", (char*) "2",
-						(char*) "--streamBgen", (char*) "data/io_test/n1000_p2000.bgen",
-						(char*) "--pheno", (char*) "data/io_test/case8/pheno.txt",
-						(char*) "--environment", (char*) "data/io_test/case8/env.txt",
-						(char*) "--out", (char*) "data/io_test/case8/test_pve_est.out.gz"};
+						(char*) "--streamBgen", (char*) "unit/data/n50_p100.bgen",
+						(char*) "--pheno", (char*) "unit/data/pheno.txt",
+						(char*) "--environment", (char*) "unit/data/n50_p100_env.txt",
+						(char*) "--out", (char*) "unit/data/test_pve/pve_est.out.gz"};
 
 
 TEST_CASE("RHE-LevenburgMarquardt") {
@@ -61,13 +61,13 @@ TEST_CASE("RHE-LevenburgMarquardt") {
 
 		pve.run();
 
-		CHECK(pve.nls_env_weights(0) == Approx(0.8369129088));
-		CHECK(pve.nls_env_weights(1) == Approx(-0.3255176523));
-		CHECK(pve.nls_env_weights(2) == Approx(0.2055679022));
+		CHECK(pve.nls_env_weights(0) == Approx(0.119065368));
+		CHECK(pve.nls_env_weights(1) == Approx(-0.3631866642));
+		CHECK(pve.nls_env_weights(2) == Approx(0.0395414866));
 
-		CHECK(pve.sigmas(0) == Approx(0.4055395441));
-		CHECK(pve.sigmas(1) == Approx(0.048474652));
-		CHECK(pve.sigmas(2) == Approx(0.3365677122));
+		CHECK(pve.sigmas(0) == Approx( 0.0043657676));
+		CHECK(pve.sigmas(1) == Approx(-0.1222527085));
+		CHECK(pve.sigmas(2) == Approx(1.1372808824));
 	}
 
 	SECTION("Detailed check"){
@@ -88,24 +88,20 @@ TEST_CASE("RHE-LevenburgMarquardt") {
 		LevenbergMarquardt LM(p, pve.components, pve.Y, pve.E, pve.C, pve.CtC_inv, pve.ytEXXtEys, pve.env_names);
 		LM.setupLM();
 		CHECK(LM.count == 0);
-		CHECK(LM.theta(0) == Approx(0.4070208966));
-		CHECK(LM.theta(1) == Approx(0.0072421961));
-		CHECK(LM.theta(2) == Approx(0.0072421961));
-		CHECK(LM.u == Approx(30.588369445));
+		CHECK(LM.theta(0) == Approx(-0.0245725812));
+		CHECK(LM.theta(2) == Approx(-0.1526502676));
+		CHECK(LM.u == Approx(2.0490523438));
 
-		CHECK(LM.JtJ(0,0) == Approx(3058.8369445019));
-		CHECK(LM.JtJ(0,1) == Approx(7.1707442261));
-		CHECK(LM.JtJ(1,1) == Approx(0.9475845954));
-		CHECK(LM.JtJ(1,2) == Approx(0.0530732073));
-		CHECK(LM.JtJ(6,6) == Approx(996.0307218357));
-		CHECK(LM.JtJ(5,6) == Approx(8.7081577861));
+		CHECK(LM.JtJ(0,0) == Approx(204.9052343808));
+		CHECK(LM.JtJ(1,1) == Approx(8.8937951504));
+		CHECK(LM.JtJ(1,2) == Approx(-0.0739273743));
 
 		LM.iterLM();
 		CHECK(LM.count == 1);
-		CHECK(LM.delta(0) == Approx(0.0000362475));
-		CHECK(LM.delta(1) == Approx(0.0680535705));
-		CHECK(LM.rho == Approx(9.1680027555));
-		CHECK(LM.u == Approx(10.1961231483));
+		CHECK(LM.delta(0) == Approx(-0.0005989636));
+		CHECK(LM.delta(1) == Approx(0.1987456184));
+		CHECK(LM.rho == Approx(0.119014003));
+		CHECK(LM.u == Approx(2.9555570866));
 		CHECK(LM.v == Approx(2.0));
 	}
 }
@@ -127,14 +123,14 @@ TEST_CASE("RHE-NelderMead") {
 		RHEreg pve(data, Y, C, data.E);
 		pve.run();
 
-		CHECK(pve.nls_env_weights(0) == Approx(0.2911621094));
-		CHECK(pve.nls_env_weights(1) == Approx(0.2802246094));
-		CHECK(pve.nls_env_weights(2) == Approx(0.2530761719));
-		CHECK(pve.nls_env_weights(3) == Approx(0.176171875));
+		CHECK(pve.nls_env_weights(0) == Approx(0.2387207031));
+		CHECK(pve.nls_env_weights(1) == Approx(0.192578125));
+		CHECK(pve.nls_env_weights(2) == Approx(0.2768066406));
+		CHECK(pve.nls_env_weights(3) == Approx(0.2814941406));
 
-		CHECK(pve.sigmas(0) == Approx(0.9156208775));
-		CHECK(pve.sigmas(1) == Approx(0.4171651285));
-		CHECK(pve.sigmas(2) == Approx(0.0932642134));
+		CHECK(pve.sigmas(0) == Approx(-0.0089448344));
+		CHECK(pve.sigmas(1) == Approx(-0.5956388748));
+		CHECK(pve.sigmas(2) == Approx(1.1600104868));
 	}
 
 	SECTION("RHEreg with env_weights from NLS") {
@@ -158,9 +154,9 @@ TEST_CASE("RHE-NelderMead") {
 		RHEreg pve(data, Y, C, data.vp_init.eta);
 		pve.run();
 
-		CHECK(pve.sigmas(0) == Approx(0.9156208775));
-		CHECK(pve.sigmas(1) == Approx(0.4171651285));
-		CHECK(pve.sigmas(2) == Approx(0.0932642134));
+		CHECK(pve.sigmas(0) == Approx(-0.0034659874));
+		CHECK(pve.sigmas(1) == Approx(-0.3798285751));
+		CHECK(pve.sigmas(2) == Approx(1.1029031174));
 	}
 }
 
@@ -186,37 +182,10 @@ TEST_CASE("RHE-multiE") {
 
 		long bb_ind = pve.n_components;
 		CHECK(pve.n_components == 3);
-		CHECK(CC(0, 0) == Approx(2642.4710418312));
-		CHECK(CC(1, 0) == Approx(1024.5639876077));
-		CHECK(CC(1, 1) == Approx(4975.4755110506));
-		CHECK(CC(1, bb_ind) == Approx(1051.0989764703));
-	}
-
-	SECTION("GxE with 5th env") {
-		parameters p;
-		int argc = sizeof(argv_multiE) / sizeof(argv_multiE[0]);
-		parse_arguments(p, argc, argv_multiE);
-		Data data(p);
-		data.read_non_genetic_data();
-		data.standardise_non_genetic_data();
-		data.read_full_bgen();
-
-		Eigen::VectorXd Y = data.Y.cast<double>();
-		Eigen::MatrixXd C = data.C.cast<double>();
-		RHEreg pve(data, Y, C, data.E.col(4));
-		pve.run();
-
-		for (long ii = 0; ii < pve.n_components; ii++) {
-			pve.components[ii].rm_jacknife_block = -1;
-		}
-		Eigen::MatrixXd CC = pve.construct_vc_system(pve.components);
-
-		long bb_ind = pve.n_components;
-		CHECK(pve.n_components == 3);
-		CHECK(CC(0, 0) == Approx(2642.4710418312));
-		CHECK(CC(1, 0) == Approx(991.4735635656));
-		CHECK(CC(1, 1) == Approx(5071.0797941864));
-		CHECK(CC(1, bb_ind) == Approx(677.3482486883));
+		CHECK(CC(0, 0) == Approx(318.6309897574));
+		CHECK(CC(1, 0) == Approx(41.4835207503));
+		CHECK(CC(1, 1) == Approx(113.3920545695));
+		CHECK(CC(1, bb_ind) == Approx(17.7540237989));
 	}
 
 	SECTION("GxE all envs") {
@@ -239,15 +208,15 @@ TEST_CASE("RHE-multiE") {
 		Eigen::MatrixXd CC = pve.construct_vc_system(pve.components);
 
 		long bb_ind = pve.n_components;
-		CHECK(pve.n_components == 7);
-		CHECK(CC(0, 0) == Approx(2642.4710418312));
+		CHECK(pve.n_components == 6);
+		CHECK(CC(0, 0) == Approx(318.6309897574));
 
-		CHECK(CC(1, 0) == Approx(1024.5639876077));
-		CHECK(CC(1, 1) == Approx(4975.4755110506));
-		CHECK(CC(1, bb_ind) == Approx(1051.0989764703));
+		CHECK(CC(1, 0) == Approx(41.4835207503));
+		CHECK(CC(1, 1) == Approx(113.3920545695));
+		CHECK(CC(1, bb_ind) == Approx(17.7540237989));
 
-		CHECK(CC(5, 0) == Approx(991.4735635656));
-		CHECK(CC(5, 5) == Approx(5071.0797941864));
-		CHECK(CC(5, bb_ind) == Approx(677.3482486883));
+		CHECK(CC(5, 0) == Approx(71.5983559289));
+		CHECK(CC(5, 5) == Approx(45.0));
+		CHECK(CC(5, bb_ind) == Approx(46.5105903879));
 	}
 }
